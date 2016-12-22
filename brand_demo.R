00dc416b742a96345fb2108797a99fa0f8fc2f0b
Robert Tooker <robert.tooker@gmail.com>
2016-12-22 09:55:09 +0000

    initial commit

---
+++ brand_demo.R
@@ -0,0 +1,33 @@
+# libraries for branding (could these be loaded via the CRUKBrand library??)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(png)
+
+#load CRUK brand defaults - need to install CRUKBrand, using devtools install_github, eg:
+# > install.packages(devtools)
+# > library(devtools)
+# > install_github("rtooker/CRUKBrand")
+library(CRUKBrand)
+
+#create some sample data
+variable1<-rnorm(200,sd=3) #200 random points from normal dist
+variable2<-variable1 * .4 + rnorm(200,sd=.5) #correlated variable
+category<-sample(c("A","B","C"),200,replace=T) #categorical variable
+
+df<-data.frame(variable1,variable2,category) #put in data frame
+
+rm(list=c("variable1","variable2","category")) #delete unneeded variables
+
+p <- NULL
+p <- qplot(x=variable1,y=variable2,data=df,color=category,geom="point") + 
+  labs(title="Bone Sarcoma (C40-C41): 1979-2013", 
+       subtitle="European Age-Standardised Incidence Rates per 100,000 Population, by Sex, Great Britain",
+       x="Year of Diagnosis",
+       y="Rate Per 100,000",
+       color="Sex")
+
+# applybrand
+g <- applyCRUKBrand(p)
+grid.draw(g)
+
